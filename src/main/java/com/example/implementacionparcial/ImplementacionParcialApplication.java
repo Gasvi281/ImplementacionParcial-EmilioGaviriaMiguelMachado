eda764aded787f3930213617650b5f5aa811afca
Gasvi281 <egaviria011@gmail.com>
2026-09-08 10:57:45 -0500

    Initial commit

---
+++ src/main/java/com/example/implementacionparcial/ImplementacionParcialApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementacionparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementacionParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementacionParcialApplication.class, args);
+    }
+
+}
